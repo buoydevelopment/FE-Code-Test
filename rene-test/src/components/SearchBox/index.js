0a706d31b90dad889bd16fc76de1fe8156cefced
Rene <rlatprnr@gmail.com>
2018-08-21 05:44:23 -0400

    reactive searchbox.

--- rene-test/src/components/SearchBox/index.js
+++ rene-test/src/components/SearchBox/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Input } from 'antd';
+import { Row, Col, Input, Icon } from 'antd';
 
 const Search = Input.Search;
 const searchBoxStyle = {
@@ -10,10 +10,11 @@ const searchBoxStyle = {
 const SearchBox = props => (
   <Row style={searchBoxStyle}>
     <Col lg={12} xs={22} offset={1}>
-      <Search
+      <Input
         placeholder={props.placeHolder}
-        onSearch={search => props.onSearch(search)}
-        enterButton
+        style={searchBoxStyle}
+        suffix={<Icon type="search" className="certain-category-icon" />}
+        onKeyDown={e => props.onSearch(e.target.value)}
       />
     </Col>
   </Row>
