818022ba9b09475bbb8a369dc2c01e1234d1ee3b
Rene <rlatprnr@gmail.com>
2018-08-21 06:20:42 -0400

    update Routes.

--- rene-test/src/routes.js
+++ rene-test/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, DefaultRoute } from 'react-router-dom';
 import App from './App';
 import CocktailList from './containers/CocktailList';
 import CocktailDetails from './containers/CocktailDetails';
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={CocktailList} />
     <Route exact path="/cocktails" component={CocktailList} />
     <Route exact path="/cocktails/:id" component={CocktailDetails} />
   </Switch>
