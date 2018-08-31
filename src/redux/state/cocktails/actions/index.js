1f1e56d88287acf87a9733dbd63d45e957df9184
ruben.daddario <ruben.daddario@gmail.com>
2018-08-31 16:11:39 -0300

    feat(cocktail-list): Use arrow function on reducer.

--- src/redux/state/cocktails/actions/index.js
+++ src/redux/state/cocktails/actions/index.js
@@ -8,85 +8,58 @@ import {
 } from 'redux/state/cocktails/constants';
 import ApiClient from 'service/api-client';
 
-export const fetchCocktails = function() {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAILS
+export const fetchCocktails = () => (dispatch) => {
+  dispatch({ type: FETCH_COCKTAILS });
+};
+
+export const fetchCocktailsSuccess = response => (dispatch) => {
+  dispatch({ type: FETCH_COCKTAILS_SUCCESS, response });
+};
+
+export const fetchCocktailsError = error => (dispatch) => {
+  dispatch({
+    type: FETCH_COCKTAILS_FAILURE,
+    error: 'Error fetching coctails'
+  });
+};
+
+export const fetchCocktail = () => (dispatch) => {
+  dispatch({ type: FETCH_COCKTAIL });
+};
+
+export const fetchCocktailSuccess = response => (dispatch) => {
+  dispatch({ type: FETCH_COCKTAIL_SUCCESS, response });
+};
+
+export const fetchCocktailError = error => (dispatch) => {
+  dispatch({
+    type: FETCH_COCKTAIL_FAILURE,
+    error: 'Error fetching coctails'
+  });
+};
+
+export const getCocktails = () => (dispatch, getState) => {
+  dispatch(fetchCocktails());
+  const url = '/api/json/v1/1/filter.php?g=Cocktail_glass';
+  ApiClient.getUrl(url)
+    .then(response => response.json())
+    .then((data) => {
+      dispatch(fetchCocktailsSuccess(data));
+    })
+    .catch((error) => {
+      dispatch(fetchCocktailsError(error));
     });
-  };
 };
 
-export const fetchCocktailsSuccess = function(response) {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAILS_SUCCESS,
-      response
+export const getCocktail = id => (dispatch) => {
+  dispatch(fetchCocktails());
+  const url = `/api/json/v1/1/lookup.php?i=${id}`;
+  ApiClient.getUrl(url)
+    .then(response => response.json())
+    .then((data) => {
+      dispatch(fetchCocktailSuccess(data));
+    })
+    .catch((error) => {
+      dispatch(fetchCocktailError(error));
     });
-  };
-};
-
-export const fetchCocktailsError = function(error) {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAILS_FAILURE,
-      error: 'Error fetching coctails'
-    });
-  };
-};
-
-export const fetchCocktail = function() {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAIL
-    });
-  };
-};
-
-export const fetchCocktailSuccess = function(response) {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAIL_SUCCESS,
-      response
-    });
-  };
-};
-
-export const fetchCocktailError = function(error) {
-  return (dispatch) => {
-    dispatch({
-      type: FETCH_COCKTAIL_FAILURE,
-      error: 'Error fetching coctails'
-    });
-  };
-};
-
-
-export const getCocktails = function() {
-  return (dispatch, getState) => {
-    dispatch(fetchCocktails());
-    const url = '/api/json/v1/1/filter.php?g=Cocktail_glass';
-    ApiClient.getUrl(url)
-      .then(response => response.json())
-      .then((data) => {
-        dispatch(fetchCocktailsSuccess(data));
-      })
-      .catch((error) => {
-        dispatch(fetchCocktailsError(error));
-      });
-  };
-};
-
-export const getCocktail = function(id) {
-  return (dispatch, getState) => {
-    dispatch(fetchCocktails());
-    const url = `/api/json/v1/1/lookup.php?i=${id}`;
-    ApiClient.getUrl(url)
-      .then(response => response.json())
-      .then((data) => {
-        dispatch(fetchCocktailSuccess(data));
-      })
-      .catch((error) => {
-        dispatch(fetchCocktailError(error));
-      });
-  };
 };
