e9fd73b5892339351ec3f8b935dfe0284e5cbc3a
rama64Dev <ramiro.ongaro@devsar.com>
2018-10-05 16:58:11 -0300

    update api, add detailDrink

--- app/src/main/java/network/api/CocktailInterfaceApi.kt
+++ app/src/main/java/network/api/CocktailInterfaceApi.kt
@@ -1,11 +1,15 @@
 package network.api
 
+import com.example.devsar.cocktailapp.home.model.DrinkDetail
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CocktailInterfaceApi {
 
     @GET("/api/json/v1/1/filter.php?g=Cocktail_glass")
     fun getDrinks(): Single<DrinksResult>
 
+    @GET("/api/json/v1/1/lookup.php{idDrink}")
+    fun getDetailDrink(@Path("idDrink") idDrink: String): Single<DrinkDetail>
 }
