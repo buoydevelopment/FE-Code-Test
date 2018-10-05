925fa063fc9dbf9ef9beff445208d8ca6ae58776
rama64Dev <ramiro.ongaro@devsar.com>
2018-10-05 14:34:24 -0300

    add general views interface

---
+++ app/src/main/java/views/CocktailView.kt
@@ -0,0 +1,15 @@
+package views
+
+import android.content.Context
+import com.example.devsar.cocktailapp.home.model.Drink
+
+interface CocktailView {
+    fun showError(error: String)
+    fun showError(error: String, throwable: Throwable)
+    fun getContext(): Context
+    fun showMoreInfo(drink:Drink)
+    fun newItem(drink: Drink)
+    fun showConnectionError()
+    fun endLoading(haveElementsToShow: Boolean)
+    fun startLoading()
+}
