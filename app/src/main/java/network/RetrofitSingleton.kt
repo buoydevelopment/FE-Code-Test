150d94ec8dd8506703b270766989595a614ee9c1
rama64Dev <ramiro.ongaro@devsar.com>
2018-10-05 16:05:35 -0300

    update dependencies Gson to moshi

--- app/src/main/java/network/RetrofitSingleton.kt
+++ app/src/main/java/network/RetrofitSingleton.kt
@@ -1,12 +1,14 @@
 package network
 
 import com.example.devsar.cocktailapp.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import network.api.CocktailInterfaceApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitSingleton  {
     private val okHttpClient = OkHttpClient().newBuilder()
@@ -18,7 +20,9 @@ object RetrofitSingleton  {
 
     private val retrofit:Retrofit= Retrofit.Builder()
                                   .baseUrl("http://www.thecocktaildb.com")
-                                  .addConverterFactory(GsonConverterFactory.create())
+                                  .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder()
+                                          .add(KotlinJsonAdapterFactory())
+                                          .build()))
                                   .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                                   .client(okHttpClient)
                                   .build()
