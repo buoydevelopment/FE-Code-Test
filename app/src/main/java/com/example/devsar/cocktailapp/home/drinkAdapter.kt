863afec65e3cb93b6db71fd636801ec866e1cd2f
rama64Dev <ramiro.ongaro@devsar.com>
2018-10-05 13:34:18 -0300

    add adapter show some data

--- app/src/main/java/com/example/devsar/cocktailapp/home/drinkAdapter.kt
+++ app/src/main/java/com/example/devsar/cocktailapp/home/drinkAdapter.kt
@@ -4,10 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.devsar.cocktailapp.R
 import com.example.devsar.cocktailapp.home.DrinkAdapter.ViewHolder
 import com.example.devsar.cocktailapp.home.model.Drink
-
+import kotlinx.android.synthetic.main.element_drink_list_info.view.*
 
 
 class DrinkAdapter: RecyclerView.Adapter<ViewHolder>() {
@@ -27,7 +29,11 @@ class DrinkAdapter: RecyclerView.Adapter<ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItem(drink: Drink) {
-
+            itemView.drinkName.text = drink.strDrink
+            Glide.with(itemView)
+                    .applyDefaultRequestOptions(RequestOptions.errorOf(R.drawable.test_drink_image))
+                    .load(drink.thumbUrl)
+                    .into(itemView.drinkImage)
         }
     }
 
