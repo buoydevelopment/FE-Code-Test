9ed9dd52be6384ff94626f709388523aa0a6d3ea
martin couso <martin.couso@gmail.com>
2019-01-08 04:58:10 -0300

    third-party code

---
+++ App/Containers/LaunchScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { ScrollView, Text, Image, View } from 'react-native'
+import { Images } from '../Themes'
+
+// Styles
+import styles from './Styles/LaunchScreenStyles'
+
+export default class LaunchScreen extends Component {
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
+      </View>
+    )
+  }
+}
---
+++ App/Sagas/index.js
@@ -0,0 +1,21 @@
+import { takeLatest, all } from 'redux-saga/effects'
+import API from '../Services/Api'
+import DebugConfig from '../Config/DebugConfig'
+
+/* ------------- Types ------------- */
+
+
+
+/* ------------- Sagas ------------- */
+
+
+
+/* ------------- API ------------- */
+
+/* ------------- Connect Types To Sagas ------------- */
+
+export default function * root () {
+  yield all([
+
+  ])
+}
