ec4788e5fe0e812920d000bed8da4e95647c2195
Fabricio Pavano <faripavano@gmail.com>
2019-04-02 23:29:20 -0300

    removed comments

--- App.js
+++ App.js
@@ -26,34 +26,3 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import {createStackNavigator, createAppContainer} from 'react-navigation';
-
-
-
-// export default App;
-
-
-
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
